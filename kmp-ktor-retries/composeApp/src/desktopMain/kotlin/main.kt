bf58912433f780953c59bac0c4db96e23cc472bb
Garth Gilmour <garth.gilmour@jetbrains.com>
2024-03-13 15:25:11 +0000

    Add demo of configuring retries in Ktor HttpClient

---
+++ kmp-ktor-retries/composeApp/src/desktopMain/kotlin/main.kt
@@ -0,0 +1,14 @@
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        state = WindowState(width = 600.dp, height = 300.dp),
+        onCloseRequest = ::exitApplication,
+        title = "kmp-ktor-retries"
+    ) {
+        App()
+    }
+}
